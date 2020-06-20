a34ad1d1c8f26159e5e6e7e0bc6570a0fd6b1832
tshibangu-jeremie <66729840+tshibangu-jeremie@users.noreply.github.com>
2020-06-20 17:02:47 +0200

    UPDATE

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,15 @@
+const hello = 'hello world'; / Exercice 1.1
+console.log(hello);
+const tshibangu = "bonjour tshibangu"; / Exercice 1.2
+console.log(jeremie);
+const fistName ="j'ai"; / Exercice 1.3
+let age = 45;
+const ans= "ans;"
+console.log(tshibangu "45 ans");
+let siriusgroup= [ "jeremie,"fanny", "lionnel", "fifi", "roger", "andre", "julien", "robert"]; / Exercice 1.4
+console.log(siriusgroup [6]); / Excercice
+console.log(siriusgroup [0]);
+let student= [student "tshibangu"; "jeremie";45]; / Excercice 1.6
+console.log(student);
+console.log(student.fisrtName) / Exrcice 1.7
+
